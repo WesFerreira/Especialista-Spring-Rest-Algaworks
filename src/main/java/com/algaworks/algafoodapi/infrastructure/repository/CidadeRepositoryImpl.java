cb0e09c16afe36a9f6433446e4beddad6b3c574f
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-01-29 17:20:19 -0300

    3.20 - mapeando entidades - desafio

--- src/main/java/com/algaworks/algafoodapi/infrastructure/repository/CidadeRepositoryImpl.java
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/repository/CidadeRepositoryImpl.java
@@ -1,9 +1,38 @@
 package com.algaworks.algafoodapi.infrastructure.repository;
 
+import com.algaworks.algafoodapi.domain.model.Cidade;
 import com.algaworks.algafoodapi.domain.repository.CidadeRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public class CidadeRepositoryImpl implements CidadeRepository {
 
+    @PersistenceContext
+    private EntityManager manager;
 
+    @Override
+    public List<Cidade> listar() {
+        return manager.createQuery("from Cidade", Cidade.class).getResultList();
+    }
 
+    @Override
+    public Cidade buscar(Long id) {
+        return manager.find(Cidade.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Cidade salvar(Cidade cidade) {
+        return manager.merge(cidade);
+    }
+
+    @Override
+    @Transactional
+    public void remover(Cidade cidade) {
+        cidade = buscar(cidade.getId());
+        manager.remove(cidade);
+    }
 }
